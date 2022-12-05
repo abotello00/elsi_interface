0c0cbfcfb165e48e0f53e969793256d5211d0a67
Uthpala Herath <ukh0001@mix.wvu.edu>
2022-12-05 14:47:36 -0500

    Reverted gnu.cmake for global access of libraries using wy29's modulefiles

--- toolchains/gnu.cmake
+++ toolchains/gnu.cmake
@@ -12,5 +12,9 @@ SET(ENABLE_PEXSI ON CACHE BOOL "Enable PEXSI")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
 
-SET(LIB_PATHS "/Users/uthpala/lib/scalapack-2.2.0/ /Users/uthpala/lib/lapack-3.9.0/" CACHE STRING "External library paths")
-SET(LIBS "libscalapack.a liblapack.a librefblas.a" CACHE STRING "External libraries")
+SET(LIB_PATHS "/home/wy29/opt/scalapack-2.1.0;/home/wy29/opt/lapack-3.9.0" CACHE STRING "External library paths")
+SET(LIBS "libscalapack.a;liblapack.a;librefblas.a" CACHE STRING "External libraries")
+
+# local
+# SET(LIB_PATHS "/Users/uthpala/lib/scalapack-2.2.0/ /Users/uthpala/lib/lapack-3.9.0/" CACHE STRING "External library paths")
+# SET(LIBS "libscalapack.a liblapack.a librefblas.a" CACHE STRING "External libraries")
