9a6e68925e1a1eec14a554a3999efffda3ed65f1
Uthpala Herath <uthpala.herath@duke.edu>
2023-07-28 01:57:36 -0400

    Test Intel and GNU with docker image.

--- toolchains/nvhpc.cmake
+++ toolchains/nvhpc.cmake
@@ -13,8 +13,7 @@ SET(ENABLE_CHASE ON CACHE BOOL "Enable ChASE eigensolver")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
 
-SET(LIB_PATHS "/home/yy244/compilers/nvhpc_2020_207_Linux_x86_64_cuda_11.0/Linux_x86_64/20.7/comm_libs/openmpi/openmpi-3.1.5/lib /home/yy244/compilers/nvhpc_2020_207_Linux_x86_64_cuda_11.0/Linux_x86_64/20.7/compilers/lib" CACHE STRING "External library paths")
-#SET(LIB_PATHS "/home/wy29/opt/nvidia/hpc_sdk/Linux_x86_64/20.7/comm_libs/openmpi/openmpi-3.1.5/lib /home/wy29/opt/nvidia/hpc_sdk/Linux_x86_64/20.7/compilers/lib" CACHE STRING "External library paths")
+SET(LIB_PATHS "/usr/local/lib" CACHE STRING "External library paths")
 SET(LIBS "scalapack lapack blas" CACHE STRING "External libraries")
 
 SET(MPIEXEC_1P "mpirun --mca io romio314 -n 1" CACHE STRING "Command to run serial tests with 1 MPI task")
