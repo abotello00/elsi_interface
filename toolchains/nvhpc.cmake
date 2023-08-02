dbbe32da5886e70a677ec307dd76f15075bec64d
Uthpala Herath <uthpala.herath@duke.edu>
2023-08-02 01:37:50 -0400

    Resolved cuda issues in nvhpc.

--- toolchains/nvhpc.cmake
+++ toolchains/nvhpc.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_Fortran_COMPILER "mpifort" CACHE STRING "MPI Fortran compiler")
 SET(CMAKE_C_COMPILER "mpicc" CACHE STRING "MPI C compiler")
 SET(CMAKE_CXX_COMPILER "mpic++" CACHE STRING "MPI C++ compiler")
 
-SET(CMAKE_Fortran_FLAGS "-O2 -Mvect=nosimd -lgfortran" CACHE STRING "Fortran flags")
+SET(CMAKE_Fortran_FLAGS "-O2 -Mvect=nosimd" CACHE STRING "Fortran flags")
 SET(CMAKE_C_FLAGS "-O1 -c99" CACHE STRING "C flags") # SCOTCH segfaults with "O2"
 SET(CMAKE_CXX_FLAGS "-O2 --c++11 -D__GCC_ATOMIC_TEST_AND_SET_TRUEVAL=1" CACHE STRING "C++ flags")
 
@@ -13,8 +13,8 @@ SET(ENABLE_CHASE ON CACHE BOOL "Enable ChASE eigensolver")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
 
-SET(LIB_PATHS "/usr/local/lib" CACHE STRING "External library paths")
+SET(LIB_PATHS "$ENV{nvcompdir}/lib $ENV{nvcommdir}/openmpi/openmpi-3.1.5/lib" CACHE STRING "External library paths")
 SET(LIBS "scalapack lapack blas" CACHE STRING "External libraries")
 
-SET(MPIEXEC_1P "mpirun --mca io romio314 -n 1" CACHE STRING "Command to run serial tests with 1 MPI task")
-SET(MPIEXEC_NP "mpirun --mca io romio314 -n 4" CACHE STRING "Command to run parallel tests with multiple MPI tasks")
+SET(MPIEXEC_1P "mpirun --allow-run-as-root --mca io romio314 -n 1" CACHE STRING "Command to run serial tests with 1 MPI task")
+SET(MPIEXEC_NP "mpirun --allow-run-as-root --mca io romio314 -n 4" CACHE STRING "Command to run parallel tests with multiple MPI tasks")
