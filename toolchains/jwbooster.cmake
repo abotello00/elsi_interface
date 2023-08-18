9c3a388afc25f1c0b060c34ca30d617fe6e116d4
Xinzhe Wu <xin.wu@fz-juelich.de>
2023-08-18 14:10:26 +0200

    update the CMake for ChASE with NCCL

--- toolchains/jwbooster.cmake
+++ toolchains/jwbooster.cmake
@@ -12,13 +12,14 @@ SET(CMAKE_CUDA_ARCHITECTURES "80" CACHE STRING "CUDA Arch")
 
 SET(USE_GPU_CUDA ON CACHE BOOL "Use CUDA-based GPU acceleration in ELPA and ChASE")
 SET(ENABLE_CHASE ON CACHE BOOL "Enable ChASE eigensolver")
+SET(ENABLE_CHASE_NCCL ON CACHE BOOL "Enable NCCL for the collective communications in ChASE-GPU")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
 
 SET(MPIEXEC_NP "srun -n 4" CACHE STRING "Command to run parallel tests with multiple MPI tasks")
 SET(MPIEXEC_1P "srun -n 1" CACHE STRING "Command to run serial tests with 1 MPI task")
 
-SET(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 $ENV{CUDA_HOME}/lib64" CACHE STRING "External library paths")
-SET(LIBS "mkl_scalapack_lp64 mkl_blacs_openmpi_lp64 mkl_gf_lp64 mkl_sequential mkl_core cublas cusolver cudart curand" CACHE STRING "External libraries")
+SET(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 $ENV{CUDA_HOME}/lib64 $ENV{EBROOTNCCL}/lib64" CACHE STRING "External library paths")
+SET(LIBS "mkl_scalapack_lp64 mkl_blacs_openmpi_lp64 mkl_gf_lp64 mkl_sequential mkl_core cublas cusolver cudart curand nccl" CACHE STRING "External libraries")
 
 
