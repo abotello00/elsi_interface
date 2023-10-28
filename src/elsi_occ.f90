9fa7ed6be55b2763a10954fb2a08288731e87a20
Uthpala Herath <uthpala.herath@duke.edu>
2023-10-28 13:15:27 -0400

    Replace method checking fractional occupancy in elsi_occ.f90

--- src/elsi_occ.f90
+++ src/elsi_occ.f90
@@ -64,12 +64,13 @@ contains
         real(kind=r8) :: lumo_occ
         real(kind=r8) :: diff
         real(kind=r8) :: occupation_def
+        real(kind=r8) :: frac_tol
         logical :: fractionally_occupied
         character(len=200) :: msg
 
         !  counters
         real*8 :: midpoint
-        integer :: i_state, i_spin, i_k_point
+        integer :: i_state, i_spin, i_k_point, i_occ_val
 
         call elsi_mu_and_occ_normal(ph,bh,n_electron,n_state,n_spin,n_kpt,k_wt,&
             eval,occ,mu)
@@ -186,18 +187,42 @@ contains
             spin_degen = ph%spin_degen
         end if
 
-        ! fractional occupation check
-        call elsi_find_homo_lumo_gap(eval, occ, n_state, n_spin, n_kpt, spin_degen, ph%flag_relativistic, homo_level, &
-            lumo_level, homo_occ, lumo_occ, dummy_int, dummy_int, dummy_int, dummy_int, dummy_log, dummy_real, &
-            dummy_int, dummy_int, dummy_int)
+        ! (OLDER) fractional occupation check
+        ! call elsi_find_homo_lumo_gap(eval, occ, n_state, n_spin, n_kpt, spin_degen, ph%flag_relativistic, homo_level, &
+        !     lumo_level, homo_occ, lumo_occ, dummy_int, dummy_int, dummy_int, dummy_int, dummy_log, dummy_real, &
+        !     dummy_int, dummy_int, dummy_int)
 
-        occupation_def = 0.05d0
+        ! occupation_def = 0.05d0
 
-        if ( (lumo_occ.ge.occupation_def) .or. (homo_occ.le.(spin_degen-occupation_def)) ) then
-            fractionally_occupied = .true.
-        else
-            fractionally_occupied = .false.
-        endif
+        ! if ( (lumo_occ.ge.occupation_def) .or. (homo_occ.le.(spin_degen-occupation_def)) ) then
+        !     fractionally_occupied = .true.
+        ! else
+        !     fractionally_occupied = .false.
+        ! endif
+
+        ! Go through all occupation numbers to see if they are fractional
+        frac_tol = 1E-13
+
+        loopi: do i_k_point = 1, n_kpt, 1
+            loopj: do i_spin = 1, n_spin, 1
+                loopk: do i_state = 1, n_state, 1
+
+                    i_occ_val = occ(i_state, i_spin,  i_k_point)
+
+                    if (abs(i_occ_val-0.0_r8) < frac_tol .or. abs(i_occ_val-spin_degen) < frac_tol) then
+                        fractionally_occupied = .false.
+                    else
+                        fractionally_occupied = .true.
+
+                        write(msg,"(A)") "ELSI found fractional occupation numbers."
+                        call elsi_say(bh,msg)
+
+                        exit loopi
+                    endif
+
+                enddo loopk
+            enddo loopj
+        enddo loopi
 
         if (fractionally_occupied .eqv. .false.) then
             homo_level = -10000000.0d0
