db82ea2d5e6fc7fb14561f19b982a58dd86b3834
Uthpala Herath <uthpala@login1.frontera.tacc.utexas.edu>
2023-11-11 13:44:47 -0600

    Disabled autotuning

--- external/ELPA-2023.05.001-LLVM/src/kernels/complex_avx_1hv_single_precision.c
+++ external/ELPA-2023.05.001-LLVM/src/kernels/complex_avx_1hv_single_precision.c
@@ -1,51 +1,3 @@
-/* Copyright 2014 - 2023, A. Marek */
-
-/*     This file is part of ELPA. */
-
-/*     The ELPA library was originally created by the ELPA consortium, */
-/*     consisting of the following organizations: */
-
-/*     - Max Planck Computing and Data Facility (MPCDF), formerly known as */
-/*       Rechenzentrum Garching der Max-Planck-Gesellschaft (RZG), */
-/*     - Bergische Universität Wuppertal, Lehrstuhl für angewandte */
-/*       Informatik, */
-/*     - Technische Universität München, Lehrstuhl für Informatik mit */
-/*       Schwerpunkt Wissenschaftliches Rechnen , */
-/*     - Fritz-Haber-Institut, Berlin, Abt. Theorie, */
-/*     - Max-Plack-Institut für Mathematik in den Naturwissenschaften, */
-/*       Leipzig, Abt. Komplexe Strukutren in Biologie und Kognition, */
-/*       and */
-/*     - IBM Deutschland GmbH */
-
-
-/*     More information can be found here: */
-/*     http://elpa.mpcdf.mpg.de/ */
-
-/*     ELPA is free software: you can redistribute it and/or modify */
-/*     it under the terms of the version 3 of the license of the */
-/*     GNU Lesser General Public License as published by the Free */
-/*     Software Foundation. */
-
-/*     ELPA is distributed in the hope that it will be useful, */
-/*     but WITHOUT ANY WARRANTY; without even the implied warranty of */
-/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
-/*     GNU Lesser General Public License for more details. */
-
-/*     You should have received a copy of the GNU Lesser General Public License */
-/*     along with ELPA.  If not, see <http://www.gnu.org/licenses/> */
-
-/*     ELPA reflects a substantial effort on the part of the original */
-/*     ELPA consortium, and we ask you to respect the spirit of the */
-/*     license that we chose: i.e., please contribute any changes you */
-/*     may have back to the original ELPA library distribution, and keep */
-/*     any derivatives of ELPA under the same license that we chose for */
-/*     the original distribution, the GNU Lesser General Public License. */
-
-/*  Author: Andreas Marek, MPCDF */
-/*  This file is the generated version. Do NOT edit */
-
-
-
 # 1 "../src/elpa2/kernels/complex_avx_1hv_single_precision.c"
 # 1 "<built-in>" 1
 # 1 "<built-in>" 3
@@ -41843,3 +41795,4 @@ static __attribute__((always_inline)) void hh_trafo_complex_kernel_4_AVX_1hv_sin
 # 9911 "../src/elpa2/kernels/complex_128bit_256bit_512bit_BLOCK_template.c"
 }
 # 55 "../src/elpa2/kernels/complex_avx_1hv_single_precision.c" 2
+
