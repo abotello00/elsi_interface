62e724405097269934d84a8fb1aa7d50c7cb6c8b
Uthpala Herath <uthpala.herath@duke.edu>
2024-04-16 14:41:39 +0000

    Updated Docker images for pipelines

--- toolchains/nvhpc.cmake
+++ toolchains/nvhpc.cmake
@@ -5,14 +5,14 @@ SET(CMAKE_C_COMPILER "mpicc" CACHE STRING "MPI C compiler")
 SET(CMAKE_CXX_COMPILER "mpic++" CACHE STRING "MPI C++ compiler")
 
 SET(CMAKE_Fortran_FLAGS "-O2 -Mvect=nosimd" CACHE STRING "Fortran flags")
-SET(CMAKE_C_FLAGS "-O1 -c99 -D_Float128=__float128" CACHE STRING "C flags") # SCOTCH segfaults with "O2"
-SET(CMAKE_CXX_FLAGS "-O2 --c++11 -D__GCC_ATOMIC_TEST_AND_SET_TRUEVAL=1 -D_Float128=__float128" CACHE STRING "C++ flags")
+SET(CMAKE_C_FLAGS "-O1 -c99" CACHE STRING "C flags") # SCOTCH segfaults with "O2"
+SET(CMAKE_CXX_FLAGS "-O2 --c++11 -D__GCC_ATOMIC_TEST_AND_SET_TRUEVAL=1" CACHE STRING "C++ flags")
 
 SET(ENABLE_PEXSI ON CACHE BOOL "Enable PEXSI")
 SET(ENABLE_CHASE ON CACHE BOOL "Enable ChASE eigensolver")
 SET(ENABLE_TESTS ON CACHE BOOL "Enable Fortran tests")
 SET(ENABLE_C_TESTS ON CACHE BOOL "Enable C tests")
-#SET(USE_ELPA_2024 ON CACHE BOOL "ENABLE ELPA 2024")
+SET(USE_ELPA_2024 ON CACHE BOOL "ENABLE ELPA 2024")
 
 SET(LIB_PATHS "$ENV{nvcompdir}/lib $ENV{nvcommdir}/openmpi/openmpi-3.1.5/lib" CACHE STRING "External library paths")
 SET(LIBS "scalapack lapack blas" CACHE STRING "External libraries")
