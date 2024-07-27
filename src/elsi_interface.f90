3bfa5b9689ed53dd5fd80436431610b707e226c3
Uthpala Herath <uthpala.herath@duke.edu>
2024-07-27 08:49:43 +0000

    Added elsi_mu_and_occ to elsi_interface.f90

--- src/elsi_interface.f90
+++ src/elsi_interface.f90
@@ -18,7 +18,7 @@ module ELSI
    use ELSI_SETUP
    use ELSI_SOLVER
    use ELSI_UTIL, only: elsi_suggest_blacs_distribution
-   use ELSI_OCC, only: elsi_check_electrons, elsi_find_homo_lumo_gap
+   use ELSI_OCC, only: elsi_check_electrons, elsi_find_homo_lumo_gap, elsi_mu_and_occ
 
    implicit none
 
